d4fa305c96d0da4c9fea69d57c0f7eb61c53ed88
Driver Station <teknights@kingsland2137.org>
2023-02-18 15:07:05 -0600

    Initial Upload robot can drive Written by @AspiringIT

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase
+{
+    CANSparkMax left_front = new CANSparkMax(3, MotorType.kBrushed);
+    CANSparkMax left_back = new CANSparkMax(2, MotorType.kBrushed);
+    CANSparkMax right_front = new CANSparkMax(5, MotorType.kBrushed);
+    CANSparkMax right_back = new CANSparkMax(4, MotorType.kBrushed);
+
+    DifferentialDrive drivetrain = new DifferentialDrive
+    (
+        new MotorControllerGroup(left_front, left_back),
+        new MotorControllerGroup(right_front, right_back)
+    );
+
+    public void drive(double xSpeed, double zRotation)
+    {
+        XboxController drivController = new XboxController(0);
+        drivetrain.arcadeDrive(-drivController.getRightX(), -drivController.getLeftY());
+    }
+}
