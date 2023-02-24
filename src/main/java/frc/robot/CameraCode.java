05c47c23fc253385a293f7aa46e2ce72d98b11af
Unc1e-Joe <98360676+Unc1e-Joe@users.noreply.github.com>
2023-02-24 17:05:42 -0600

    Update CameraCode.java

--- src/main/java/frc/robot/CameraCode.java
+++ src/main/java/frc/robot/CameraCode.java
@@ -1,5 +1,6 @@
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
+
 /**
  * Uses the CameraServer class to automatically capture video from a USB webcam and send it to the
  * FRC dashboard without doing any vision processing. This is the easiest way to get camera images
