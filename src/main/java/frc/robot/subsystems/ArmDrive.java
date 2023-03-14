1fcd9d019753287c916f9c10291187dfb7ceb694
AspiringIT <nicktab20@gmail.com>
2023-03-14 16:36:35 -0500

    workin on pid

--- src/main/java/frc/robot/subsystems/ArmDrive.java
+++ src/main/java/frc/robot/subsystems/ArmDrive.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import edu.wpi.first.math.MathUtil;
@@ -25,6 +26,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.SparkMaxAbsoluteEncoder.*; 
 import com.revrobotics.SparkMaxPIDController; 
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax.ControlType.*;
+//remember * is a wildcasrd that inports everthing to do with what it's calling just not one specific things but everything to do with what its calling
+
 
 
 
@@ -42,6 +46,7 @@ public void Arm_Drive()
   m_pid.setFeedbackDevice(m_enc);
   ArmSpeed = armrightydeadband;
     ArmDriveMotor.set(-armrightydeadband);
+    double rotations = m_enc.getPosition();
     //Proportional
     double kP = 0.1; 
     //Integral
@@ -54,6 +59,44 @@ public void Arm_Drive()
     double kFF = 0; 
     double kMaxOutput = 0.25; 
     double kMinOutput = -0.25;
+    // set PID coefficients
+    m_pid.setP(kP);
+    m_pid.setI(kI);
+    m_pid.setD(kD);
+    m_pid.setIZone(kIz);
+    m_pid.setFF(kFF);
+    m_pid.setOutputRange(kMinOutput, kMaxOutput);
+        // display PID coefficients on SmartDashboard
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+        SmartDashboard.putNumber("inital Rotations", 0);
+
+        m_pid.setReference(rotations, CANSparkMax.ControlType.kPosition);
+
+        SmartDashboard.putNumber("SetPoint", rotations);
+        SmartDashboard.putNumber("ProcessVariable", m_enc.getPosition());
+
+        /**
+     * Encoder position is read from a RelativeEncoder object by calling the
+     * GetPosition() method.
+     * 
+     * GetPosition() returns the position of the encoder in units of revolutions
+     */
+    SmartDashboard.putNumber("Encoder Position", m_enc.getPosition());
+
+    /**
+     * Encoder velocity is read from a RelativeEncoder object by calling the
+     * GetVelocity() method.
+     * 
+     * GetVelocity() returns the velocity of the encoder in units of RPM
+     */
+    SmartDashboard.putNumber("Encoder Velocity", m_enc.getVelocity());
+
 }
 
 
