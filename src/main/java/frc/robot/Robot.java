77635381da00adc61e2d1e0f7a4b993a907cec34
AspiringIT <nicktab20@gmail.com>
2023-03-20 18:12:36 -0500

    Worked on Lights

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,7 +52,7 @@ public class Robot extends TimedRobot {
    // Reuse buffer
     // Default to a length of 60, start empty output
     // Length is expensive to set, so only set it once, then just update data
-   AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(60);
+   AddressableLEDBuffer m_ledBuffer = new AddressableLEDBuffer(150);
     m_led.setLength(m_ledBuffer.getLength());
 
     // Set the data
@@ -156,5 +156,20 @@ public class Robot extends TimedRobot {
   public void setServer(CameraServer server) {
     this.server = server;
   }
+  private void rainbow() { /*/
+        // For every pixel
+        for (var i = 0; i < Robot.m_ledBuffer.getLength(); i++) {
+          // Calculate the hue - hue is easier for rainbows because the color
+          // shape is a circle so only one value needs to precess
+          final var hue = (m_rainbowFirstPixelHue + (i * 180 / m_ledBuffer.getLength())) % 180;
+         // Set the value
+          m_ledBuffer.setHSV(i, hue, 255, 128);
+       }
+        // Increase by to make the rainbow "move"
+        m_rainbowFirstPixelHue += 3;
+       // Check bounds
+        m_rainbowFirstPixelHue %= 180; */
+      }
+      
 
 }
